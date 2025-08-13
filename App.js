838ae20223c702658fbe3c48cc4b1556ee080b82
ayselcetin07 <ayselcetin07@hotmail.com>
2025-08-13 14:22:50 +0300

    MapViewScreen screens klasörüne taşındı

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { SafeAreaView, StyleSheet } from "react-native";
 import { LocationProvider } from "./context/LocationContext";
 import AddressInput from "./components/AddressInput";
 import AddressList from "./components/AddressList";
-import MapViewScreen from "./components/MapViewScreen";
+import MapViewScreen from "./screens/MapViewScreen";
 
 const App = () => {
   return (
